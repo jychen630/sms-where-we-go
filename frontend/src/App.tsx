ad184d94e09319e4bf2b126a97073386c83b89b3
jychen630 <jychen630@outlook.com>
2021-05-20 18:20:10 +0800

    router: Add routers in App.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,12 @@ import React, { useEffect, useRef } from 'react';
 import logo from './logo.svg';
 import mapboxgl from 'mapbox-gl';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect
+} from "react-router-dom";
 import Map from './Map';
 
 mapboxgl.accessToken = "pk.eyJ1IjoiYWNraW5kbGUzIiwiYSI6ImNrYzYzcWFnOTA5bjQycnRlY2t4OWxlMWUifQ.a3wwi4cHq1sHakuoT9Bo0w";
@@ -9,9 +15,20 @@ mapboxgl.accessToken = "pk.eyJ1IjoiYWNraW5kbGUzIiwiYSI6ImNrYzYzcWFnOTA5bjQycnRlY
 function App() {
 
   return (
-    <div className="App">
-      <Map />
-    </div>
+    <Router>
+
+      <Switch>
+        <Route path="/login">
+          login
+        </Route>
+        <Route path="/register">register</Route>
+        <Route path="/map">
+          <Map />
+        </Route>
+        <Route path="/roster">roster</Route>
+        <Route path="/"><Redirect to="/login"/></Route>
+      </Switch>
+    </Router>
   );
 }
 
