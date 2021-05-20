ae2007e8833acffca5f834ede338f32ae7167994
jychen630 <jychen630@outlook.com>
2021-05-20 16:26:07 +0800

    frontend: Set up Mapbox.

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,27 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import logo from './logo.svg';
+import mapboxgl from 'mapbox-gl';
 import './App.css';
 
+
+mapboxgl.accessToken = "pk.eyJ1IjoiYWNraW5kbGUzIiwiYSI6ImNrYzYzcWFnOTA5bjQycnRlY2t4OWxlMWUifQ.a3wwi4cHq1sHakuoT9Bo0w";
+
 function App() {
+  const mapContainer = useRef(null);
+  const map = useRef(null);
+  useEffect(() => {
+    if (!!map.current && !!mapContainer.current) return;
+    if (map.current) return; // initialize map only once
+    // @ts-ignore
+    map.current = new mapboxgl.Map({  
+      // @ts-ignore
+      container: mapContainer.current,
+      style: 'mapbox://styles/mapbox/streets-v11',
+    });
+  });
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learssn Rea
-        </a>
-      </header>
+      <div className="map-container" ref={mapContainer}></div>
     </div>
   );
 }
