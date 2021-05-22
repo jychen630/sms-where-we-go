2b4e6045aa6b8184f75841025d6b3226af9d1fd3
PIG208 <359101898@qq.com>
2021-05-22 21:31:50 +0800

    map: Support Chinese by default.

--- frontend/src/Map.tsx
+++ frontend/src/Map.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useRef } from 'react';
 import mapboxgl from 'mapbox-gl';
+import MapboxLanguage from '@mapbox/mapbox-gl-language';
 import './Map.css';
 
 mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_TOKEN as string;
@@ -10,10 +11,11 @@ export default function Map() {
     useEffect(() => {
         if (!!map.current && !!mapContainer.current) return;
         if (map.current) return; // initialize map only once
+        let lang = new MapboxLanguage({ defaultLanguage: 'zh', supportedLanguages: ['zh', 'en'] });
         map.current = new mapboxgl.Map({
             container: mapContainer.current as any,
-            style: 'mapbox://styles/mapbox/streets-v11',
-        }) as any;
+            style: 'mapbox://styles/mapbox/streets-v10',
+        }).addControl(lang) as any;
     });
 
     return <div className='map-container' ref={mapContainer}></div>
