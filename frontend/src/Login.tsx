fd88b4637d342bff3b97341889ae92e160763def
jychen630 <jychen630@outlook.com>
2021-05-24 12:52:23 +0800

    style: Adjust the style in Register and Login pages

--- frontend/src/Login.tsx
+++ frontend/src/Login.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useRef } from 'react';
 import Form from 'react-bootstrap/Form';
+import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Card from 'react-bootstrap/Card';
@@ -14,23 +15,24 @@ function Login() {
         formState: { errors }
     } = useForm<LoginForm>();
 
-    const onSubmit = (data: LoginForm) => { 
+    const onSubmit = (data: LoginForm) => {
         console.log(data);
     };
 
     return (
         <div className="Login">
-            <Card style={{ width: '18rem' }}>
+            <Card>
+                <Card.Header as="h5">登录</Card.Header>
                 <Card.Body>
-                    <Card.Title>登录</Card.Title>
+
                     <Form onSubmit={handleSubmit(onSubmit)} >
                         <MyFormInput
                             control={control as any}
                             name="identifier"
-                            displayName="电话号码/邮箱"
+                            displayName="手机号码/邮箱"
                             error={errors.identifier}
                             errorMessage="此项为必填"
-                            placeholder="请输入电话号码"
+                            placeholder="请输入手机号码"
                             required={true}
                         />
 
@@ -38,30 +40,34 @@ function Login() {
                             control={control as any}
                             name="password"
                             displayName="密码"
-                            controlProps={{type:'password'}}
+                            controlProps={{ type: 'password' }}
                             error={errors.password}
                             errorMessage="此项为必填"
                             placeholder="请输入密码"
                             required={true}
                         />
-                        
+
                         <MyFormInput
                             control={control as any}
                             name="rememberMe"
-                            displayName="自动登录"
+                            displayName=""
                             required={false}
                             altRender={
-                                ({field: {onChange, value}}) => (
+                                ({ field: { onChange, value } }) => (
                                     <Form.Check onChange={onChange} value={value} type="checkbox" label="自动登录" />
                                 )
                             }
                         />
-                        <Button variant="primary">
-                            去注册
-                        </Button>
-                        <Button variant="primary" type="submit">
-                            登录
-                        </Button>
+                        <Form.Row>
+                            <Col><Button variant="primary" type="submit">
+                                登录
+                        </Button></Col>
+                            <Col> <Button variant="primary">
+                                去注册
+                        </Button></Col>
+                        </Form.Row>
+
+
                     </Form>
 
                 </Card.Body>
--- frontend/src/Register.tsx
+++ frontend/src/Register.tsx
@@ -1,4 +1,5 @@
 import Form from 'react-bootstrap/Form';
+import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Card from 'react-bootstrap/Card';
@@ -14,96 +15,109 @@ function Register() {
         watch
     } = useForm();
 
-    const onSubmit = (data: any) => { 
+    const onSubmit = (data: any) => {
         console.log(data);
     };
 
     const watchSchoolName = watch("schoolName");
 
     return (
-        <div className="Register">
-            <Card style={{ width: '18rem' }}>
-                <Card.Body>
-                    <Card.Title>注册</Card.Title>
-                    <Form onSubmit={handleSubmit(onSubmit)} >
-                        <MyFormInput
-                            control={control as any}
-                            name="name"
-                            displayName="姓名"
-                            error={errors.name}
-                            errorMessage="此项为必填"
-                            placeholder="请输入姓名"
-                            required={true}
-                        />
+        <div className="Register"> 
+        <Card>
+            <Card.Header as="h5">注册</Card.Header>
+            <Card.Body>
 
-                        <MyFormInput
-                            control={control as any}
-                            name="registration_key"
-                            displayName="注册码"
-                            error={errors.registration_key}
-                            errorMessage="此项为必填"
-                            placeholder="请输入注册码（where）"
-                            required={true}
-                        />
-                        
-                        <MyFormInput
-                            control={control as any}
-                            name="phone_number"
-                            displayName="请输入电话号码（选填）"
-                            required={false}
-                            
-                        />
-                        <MyFormInput
-                            control={control as any}
-                            name="email"
-                            displayName="请输入邮箱（选填）"
-                            required={false}
-                            
-                        />
+                <Form onSubmit={handleSubmit(onSubmit)} >
+                    <MyFormInput
+                        control={control as any}
+                        name="name"
+                        displayName="姓名"
+                        error={errors.name}
+                        errorMessage="此项为必填"
+                        placeholder="请输入姓名"
+                        required={true}
+                    />
 
-                        <MyFormInput
-                            control={control as any}
-                            name="wxid"
-                            displayName="请输入微信账号（选填）"
-                            required={false}
-                            
-                        /><MyFormInput
-                            control={control as any}
-                            name="schoolName"
-                            displayName="学校"
-                            required={false}
-                            controlProps={{className: "search-input"}}
-                        />
-                          {watchSchoolName &&
+                    <MyFormInput
+                        control={control as any}
+                        name="registration_key"
+                        displayName="注册码"
+                        error={errors.registration_key}
+                        errorMessage="此项为必填"
+                        placeholder="请输入注册码（where）"
+                        required={true}
+                    />
+
+                    <MyFormInput
+                        control={control as any}
+                        name="phone_number"
+                        displayName="请输入手机号码（选填）"
+                        placeholder="186******60"
+                        required={false}
+
+                    />
+                    <MyFormInput
+                        control={control as any}
+                        name="email"
+                        displayName="请输入邮箱（选填）"
+                        placeholder="abc@123.com"
+                        required={false}
+
+                    />
+
+                    <MyFormInput
+                        control={control as any}
+                        name="wxid"
+                        displayName="请输入微信账号（选填）"
+                        placeholder="wxid123"
+                        required={false}
+
+                    /><MyFormInput
+                        control={control as any}
+                        name="schoolName"
+                        displayName="就读大学"
+                        required={false}
+                        controlProps={{ className: "search-input" }}
+                    />
+                    {watchSchoolName &&
                         <ListGroup className="dropdown">
                             <ListGroup.Item action>Cras justo odio</ListGroup.Item>
                             <ListGroup.Item action>Dapibus ac facilisis in</ListGroup.Item>
-                            </ListGroup>
-}
-                        <MyFormInput
-                            control={control as any}
-                            name="wxid"
-                            displayName="请输入学院名称（选填）"
-                            required={false}
-                            
-                        />
-                        <MyFormInput
-                            control={control as any}
-                            name="wxid"
-                            displayName="请输入专业（选填）"
-                            required={false}
-                            
-                        />
-                        <Button variant="primary">
-                            注册
-                        </Button>
-                        <Button variant="primary" type="submit">
-                            已注册，去登录
-                        </Button>
-                    </Form>
+                        </ListGroup>
+                    }
+                    <MyFormInput
+                        control={control as any}
+                        name="wxid"
+                        displayName="请输入学院名称（选填）"
+                        placeholder=""
+                        required={false}
 
-                </Card.Body>
-            </Card>
+                    />
+                    <MyFormInput
+                        control={control as any}
+                        name="wxid"
+                        displayName="请输入专业全称（选填）"
+                        required={false}
+
+                    />
+                    <Form.Row>
+                        <Col>
+                            <Button variant="primary">
+                                注册
+                        </Button>
+                        </Col>
+                        <Col>
+                            <Button variant="primary" type="submit">
+                                已注册，去登录
+                        </Button>
+                        </Col>
+                    </Form.Row>
+
+
+                </Form>
+
+            </Card.Body>
+        </Card>
         </div>
     );
 }
