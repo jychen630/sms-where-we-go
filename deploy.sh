2cef5592554d72d1da1dc25e1598ffa9a4fd2781
PIG208 <359101898@qq.com>
2021-05-26 00:51:35 +0800

    tooling: Do not rerun setup when deploying but generate ts instead.

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,11 @@
 PARENT_PATH=$(cd "$(dirname "{BASH_SOURCE[0]}")"; pwd -P)
 cd $PARENT_PATH
 
-sh setup.sh
 cd frontend
 yarn start &
 (
     cd ../backend
+    yarn generate
     yarn deploy
 ) && fg
 wait
