64edabff5b524c7d4853341662a74eca9c78a3c5
PIG208 <359101898@qq.com>
2021-05-26 06:32:55 +0800

    db: Add more mock data.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -14,29 +14,94 @@ const port = 8080;
 
 export const pg = knex(pgOptions);
 export const populateTestData = async () => {
-    await pg('wwg.curriculum').insert({
+    await pg('wwg.curriculum').insert([{
         name: "international"
-    });
-    await pg('wwg.class').insert({
+    }, {
+        name: "gaokao"
+    }]);
+    await pg('wwg.class').insert([{
         class_number: 2,
         grad_year: 2019,
         curriculum_uid: 1
-    });
+    }, {
+        class_number: 3,
+        grad_year: 2019,
+        curriculum_uid: 1
+    }, {
+        class_number: 2,
+        grad_year: 2020,
+        curriculum_uid: 1
+    }, {
+        class_number: 4,
+        grad_year: 2019,
+        curriculum_uid: 2
+    }, {
+        class_number: 5,
+        grad_year: 2019,
+        curriculum_uid: 2
+    }]);
     await pg('wwg.school').insert({
         name: "Test School",
         position: pg.raw('point(34,-34)'),
         country: "United States",
         city: "test city"
     });
-    await pg('wwg.student').insert({
-        name: "test_std",
+    await pg('wwg.student').insert([{
+        name: "Ming",
         phone_number: "18923232323",
         password_hash: "$2b$10$5uAd2PJztsBwdXoYsMeo2e5kUJ7kC5XLxV5URwbpagP3ibVUjnNyK",
         class_number: 2,
         grad_year: 2019,
-        curriculum_uid: 1,
         school_uid: 1
-    });
+    }, {
+        name: "Dan",
+        phone_number: "13988889999",
+        password_hash: "$2b$10$5uAd2PJztsBwdXoYsMeo2e5kUJ7kC5XLxV5URwbpagP3ibVUjnNyK",
+        class_number: 2,
+        grad_year: 2019,
+        school_uid: 1,
+        visibility_type: 'class'
+    }, {
+        name: "Kang",
+        phone_number: "13634343434",
+        password_hash: "$2b$10$5uAd2PJztsBwdXoYsMeo2e5kUJ7kC5XLxV5URwbpagP3ibVUjnNyK",
+        class_number: 2,
+        grad_year: 2020,
+        school_uid: 1,
+        visibility_type: 'curriculum'
+    }, {
+        name: "Wang",
+        phone_number: "18612344321",
+        password_hash: "$2b$10$5uAd2PJztsBwdXoYsMeo2e5kUJ7kC5XLxV5URwbpagP3ibVUjnNyK",
+        class_number: 2,
+        grad_year: 2020,
+        school_uid: 1,
+        visibility_type: 'curriculum'
+    }, {
+        name: "Fang",
+        phone_number: "13900002222",
+        password_hash: "$2b$10$5uAd2PJztsBwdXoYsMeo2e5kUJ7kC5XLxV5URwbpagP3ibVUjnNyK",
+        class_number: 3,
+        grad_year: 2019,
+        school_uid: 1,
+        visibility_type: 'private'
+    }, {
+        name: "Zheng",
+        phone_number: "13900006666",
+        password_hash: "$2b$10$5uAd2PJztsBwdXoYsMeo2e5kUJ7kC5XLxV5URwbpagP3ibVUjnNyK",
+        class_number: 4,
+        grad_year: 2019,
+        school_uid: 1,
+        visibility_type: 'curriculum'
+    }, {
+        name: "Gao",
+        phone_number: "18912346666",
+        password_hash: "$2b$10$5uAd2PJztsBwdXoYsMeo2e5kUJ7kC5XLxV5URwbpagP3ibVUjnNyK",
+        class_number: 5,
+        grad_year: 2019,
+        school_uid: 1,
+        visibility_type: 'curriculum'
+    }]);
     await pg('wwg.registration_key').insert({
         registration_key: "wwgasdf",
         expiration_date: new Date('2022').toISOString(),
