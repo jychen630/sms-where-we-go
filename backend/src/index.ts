22a809f39da42fb74eb6831455531a613222f6d7
PIG208 <359101898@qq.com>
2021-05-28 02:30:19 +0800

    api: Handle oneOf error for request validations.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -155,7 +155,7 @@ initialize({
         'application/json': express.json()
     },
     errorMiddleware: function (err: any, req: any, res: any, next) {
-        logger.info(err);
+        logger.error(err);
 
         if (err.type === 'entity.parse.failed') {
             sendError(res, err.status, 'The content format is invalid');
@@ -170,7 +170,17 @@ initialize({
         const error = err.errors[0];
         const errCode = (error.errorCode as string).match(validationPattern);
         if (errCode) {
-            sendError(res, err.status ?? 400, `[${errCode[1]}] "${error.path}" ${error.message}`);
+            if (errCode[1] === 'oneOf' && req.path === '/student' && req.method === 'POST') {
+                sendError(res, err.status ?? 400, 'You should either pass a registration key or class number with graduation year');
+                return;
+            }
+
+            if (error.path !== undefined) {
+                sendError(res, err.status ?? 400, `[${errCode[1]}] "${error.path}" ${error.message}`);
+            }
+            else {
+                sendError(res, err.status ?? 400, `[${errCode[1]}] ${error.message}`);
+            }
         }
         else {
             sendError(res, err.status ?? 400, error.message ?? 'The input information is invalid');
