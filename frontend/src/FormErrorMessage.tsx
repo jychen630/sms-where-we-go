b0cbd8dfddc256189f8de6ce7dbc9c6ccfec3c8f
jychen630 <jychen630@outlook.com>
2021-05-29 14:03:22 +0800

    frontend: Discard react-boostrap; add antd.

--- frontend/src/FormErrorMessage.tsx
+++ frontend/src/FormErrorMessage.tsx
@@ -1,14 +1,17 @@
-import Form from "react-bootstrap/Form";
+// //import Form from "react-bootstrap/Form";
+// import { Form } from 'antd';
 
-const FormErrorMessage = (props: { message?: string }) => {
-  if (props.message === undefined || props.message.length === 0) {
-    return <div></div>;
-  }
-  return (
-    <Form.Control.Feedback type="invalid" style={{ display: "block" }}>
-      {props.message}
-    </Form.Control.Feedback>
-  );
-};
 
-export default FormErrorMessage;
+
+// const FormErrorMessage = (props: { message?: string }) => {
+//   if (props.message === undefined || props.message.length === 0) {
+//     return <div></div>;
+//   }
+//   return (
+//     <Form.Item>
+//       {props.message}
+//     </Form.Item>
+//   );
+// };
+
+// export default FormErrorMessage;
