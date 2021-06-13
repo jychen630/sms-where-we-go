15dc2641bb43c70abf7016b51b583ddaa24ef663
PIG208 <359101898@qq.com>
2021-06-13 23:55:33 +0800

    utils: Implement factory function for notifying users errors.

---
+++ frontend/src/api/utils.tsx
@@ -0,0 +1,66 @@
+import { notification } from 'antd';
+import React from 'react';
+import { ApiError, Result } from 'wwg-api';
+
+
+type ProcessedError = Result & { requireLogin: boolean };
+/**
+ * A type-safe helper to convert err of `any` type to standard `Result` type
+ * @param err 
+ * @returns 
+ */
+export const handleApiError = async (err: ApiError | string | undefined, cb?: (props: ProcessedError) => void): Promise<ProcessedError> => {
+    let requireLogin = false;
+    let error = {
+        result: Result.result.ERROR,
+        message: '发生了未知错误',
+        requireLogin: requireLogin
+    };
+    if (typeof err === 'string') {
+        error = {
+            result: Result.result.ERROR,
+            message: err,
+            requireLogin: requireLogin
+        };
+    }
+    else if (err === undefined) {
+        error = {
+            result: Result.result.ERROR,
+            message: '发生了未知错误',
+            requireLogin: requireLogin
+        };
+    }
+    else if (err.status === 401) {
+        requireLogin = true;
+    }
+
+    else if (!!err.body) {
+        error = {
+            result: err.body.result ?? Result.result.ERROR,
+            message: err.body.message ?? '发生了未知错误',
+            requireLogin: requireLogin
+        };
+    }
+    cb && cb(error);
+    return error;
+}
+
+export const createNotifyError = (title?: string, errorMsg?: string, cb?: () => void): (props: ProcessedError) => void => {
+    /**
+     * Create a notify error function that can be used as the callback for handleApiError
+     * @param title The title of the notification message
+     * @param errorMsg The error message of the notification message
+     * @param cb A callback function that takes no argument
+     */
+    return (props) => {
+        notification.error({
+            message: title,
+            description: <><p>{props.message}</p>{errorMsg && <p>错误信息: {errorMsg}</p>}</>
+        });
+        cb && cb();
+    };
+}
+
+export type HasChildren<T = {}> = T & { children: React.ReactNode };
+// Reference: https://stackoverflow.com/a/49889856/11612399
+export type ThenType<T> = T extends PromiseLike<infer U> ? U : T;
