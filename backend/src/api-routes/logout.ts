76ebdb58f9d8ad0466eef8282ce46a3cd4e92ff1
PIG208 <359101898@qq.com>
2021-06-29 23:05:49 +0800

    api: Implement logout.

---
+++ backend/src/api-routes/logout.ts
@@ -0,0 +1,21 @@
+import { Operation } from "express-openapi";
+import { Actions, sendError, sendSuccess, ServerLogger, validateLogin } from "../utils";
+
+export const get: Operation = (req, res) => {
+    const logger = ServerLogger.getLogger('logout');
+
+    if (!validateLogin(req, res, logger)) return;
+
+    const identifier = req.session.identifier;
+
+    req.session.destroy((err) => {
+        if (!!err) {
+            logger.logComposed(identifier, Actions.access, 'logout', false, err, true, err);
+            sendError(res, 500, "Failed to logout");
+        }
+        else {
+            logger.logComposed(identifier, Actions.access, 'logout');
+            sendSuccess(res);
+        }
+    })
+}
