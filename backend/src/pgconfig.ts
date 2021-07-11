2631bb8207d8e1ecf551d428fda174efa9ccc0c7
PIG208 <359101898@qq.com>
2021-07-11 23:55:53 +0800

    db: Use different db in development.

--- backend/src/pgconfig.ts
+++ backend/src/pgconfig.ts
@@ -1,13 +1,14 @@
 import dotenv from 'dotenv';
 
 dotenv.config();
+console.log(`Using db ${process.env.NODE_ENV === 'production' ? 'wwg_base' : 'development'}.`);
 export const pgOptions = {
     client: 'pg',
     connection: {
         host: process.env.PGHOST,
         user: process.env.PGUSER,
         password: process.env.PGPASSWORD,
-        database: 'wwg_base'
+        database: process.env.NODE_ENV === 'production' ? 'wwg_base' : 'development'
     },
     searchPath: ['wwg', 'public'],
 };
