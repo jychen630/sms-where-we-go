f0055d8fb6b5bfdcdfe2b156d9dc71d0215c638f
Zixuan James Li <359101898@qq.com>
2022-01-14 15:00:42 -0500

    setup: Fix path in setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,8 @@
-PARENT_PATH=$(cd "$(dirname "{BASH_SOURCE[0]}")"; pwd -P)
-cd $PARENT_PATH
-
 echo "[setup] Setting up secret files..."
-mkdir -p ../secrets
-cd ../secrets
+mkdir -p secrets
+cd secrets
 touch amap_token pg_password mapbox_token express_session_secret
 
 echo "[setup] Pulling images"
-docker compose pull
+cd ..
+docker-compose pull
